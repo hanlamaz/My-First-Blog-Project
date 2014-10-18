a968f39d69abc1e93788975114ea0533591fab69
Hüseyin ANLAMAZ <hanlamaz13@posta.pau.edu.tr>
2014-10-18 23:47:10 +0300

    Blog
    Hello World project

---
+++ MyFirstBlogProject/Source.cpp
@@ -0,0 +1,10 @@
+// Hello World
+#include <stdio.h> // Stdio kütüphanesi
+#include <conio.h> // Conio kütüphanesi ( Progeam bittikten sonra iþlemi bekletir )
+int main()
+{
+	
+	printf("Hello World"); // Printf komutu konsolda yazýlan ismi gösterir
+	_getch(); // getch komutu program bittikten sonra bir tuþa basýlmasýný söyler
+
+}
